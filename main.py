1e6275a4abfb6d267ae2aaeebfaa4bbf6c5438ea
GURUPHAT NICOLAS p2003245 <nicolas.guruphat@etu.univ-lyon1.fr>
2021-11-25 12:03:56 +0000

    ajout de ce qu'on a fait en cours à la séance 1

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
+from tkinter import *
+class game:
+	def __init__(self):
+		interface=UI()
+		#instaciate other stuff
+		interface.root.mainloop()
 
+
+
+class UI:
+
+	def __init__(self):
+		self.root = Tk();
+		self.button
+		self.fill()
+	def fill(self):
+		self.button = Button(text="Play",background="lightgreen")
+
+		self.button.grid(row=0,column=0)
+
+Game=game()
