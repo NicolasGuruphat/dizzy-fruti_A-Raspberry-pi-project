90aa1b39a5240b6af4f58b00f2e63eedaad2033c
NicolasGuruphat <nicolas.guruphat@gmail.com>
2021-11-25 13:29:14 +0100

    Ajout de méthodes dans la classe UI et dans la classe Game play = méthode static qui lance le jeu

--- main.py
+++ main.py
@@ -1,21 +1,36 @@
 from tkinter import *
-class game:
+
+class Game:
 	def __init__(self):
 		interface=UI()
 		#instaciate other stuff
 		interface.root.mainloop()
-
+		
+	@staticmethod
+	def play():
+		print("jeu lancé")
 
 
 class UI:
 
 	def __init__(self):
-		self.root = Tk();
-		self.button
+		
+		self.root = Tk()
+		self.listComposant=[]
+		self.buttonPlay = Button(text="Play",background="lightgreen", command=Game.play())
 		self.fill()
-	def fill(self):
-		self.button = Button(text="Play",background="lightgreen")
 
-		self.button.grid(row=0,column=0)
-print("test forge")
-Game=game()
+	def fill(self): 
+		self.buttonPlay.grid(row=0,column=0)
+		self.addToList(self.buttonPlay)
+
+
+	def addToList(self,composant):
+		self.listComposant.append(composant)
+
+	def flush(self):
+		for composant in self.listeComposant:
+			composant.destroy()
+
+
+game=Game()
