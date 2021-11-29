72d9353600479ed5922e5ffbcba8f82234d8a4f8
“Valette_Adrien” <adrien.valette@etu.univ-lyon1.fr>
2021-11-29 21:09:04 +0100

    Adding  fruitFactory function in Game.py and Fruit class

---
+++ Fruit.py
@@ -0,0 +1,47 @@
+from tkinter import *
+from PIL import ImageTk,Image
+from random import *
+
+class Fruit : 
+	
+	def __init__(self,menu): 
+		randomInt=randint(1,100)
+		self.menu=menu
+		self.TkMenu=menu.TkMenu
+		self.canvas=self.menu.canvas
+
+		if 1 <= randomInt <= 10 : 
+			path = "sprite/sprite_strawberry.png"
+			self.taille = 24
+	        
+
+		if 11 <= randomInt <= 30 :
+			path = "sprite/sprite_lemon.png"
+			self.taille = 32
+	        
+
+		if 31 <= randomInt <= 70 :
+			path = "sprite/sprite_apple.png"
+			self.taille = 48
+	       
+
+		if 71 <= randomInt <= 90 :
+			path = "sprite/sprite_orange.png"
+			self.taille = 64
+
+		if 91 <= randomInt <= 100 :
+			path = "sprite/sprite_watermelon.png"
+			self.taille = 96
+
+
+
+
+		self.imageFruit = ImageTk.PhotoImage(master=self.TkMenu,file=path)
+		self.canvas.image=self.imageFruit
+		self.canvas.grid(row=0,column=0)
+		self.fuitItem=self.canvas.create_image(0,0, anchor=NW,image = self.canvas.image)
+
+		randomInt=randint(0,menu.taille[0]-(self.taille))
+		
+		self.canvas.moveto(self.fuitItem,randomInt,0) #set random x position on top of the screen 
+
--- Game.py
+++ Game.py
@@ -1,23 +1,42 @@
 from tkinter import *
 from UI import UI
 from Bowl import Bowl
+from Fruit import Fruit
 
 class Game:
 
+
+		
+		
+
 	def __init__(self):
+		
 		self.interface=UI(self)
 		self.interface.displayMainMenu()
 		self.interface.root.mainloop()
+
+
+
+	def fruitFactory(self) : 
+		print("132")
+		self.listFruit.append(Fruit(self.interface.getMenu()))
+		self.interface.getMenu().TkMenu.after(1000,self.fruitFactory())
 		
+
 	
 	def play(self):
 		print("Play !")
+		self.inGame = True
 		self.interface.hideMainMenu()
 		self.interface.displayGameMenu()
 		self.bowl=Bowl(self.interface.getMenu())
 		self.interface.getMenu().TkMenu.bind('<KeyPress-Left>',lambda event :self.bowl.move("left"))
 		self.interface.getMenu().TkMenu.bind('<KeyPress-Right>',lambda event :self.bowl.move("right"))
-  
-		
+		self.listFruit=[]
+
 
 game=Game()
+
+
+
+
