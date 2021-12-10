b99de905fcaa63cd727a787feedc5ffdda2a8ae7
NicolasGuruphat <nicolas.guruphat@gmail.com>
2021-12-10 01:09:02 +0100

    Converts indentation to tabs

--- Menu.py
+++ Menu.py
@@ -1,36 +1,36 @@
 from tkinter import Tk, Button, Canvas, Label
 
 class Menu:
-    def __init__(self,game,title):
-        self.game=game
-        self.listComponant=[]
-        self.TkMenu=Tk()
-        self.TkMenu.withdraw()
-        self.TkMenu.title(title)
-        self.taille=[720,480]
-        self.TkMenu.minsize(self.taille[0],self.taille[1])
+	def __init__(self,game,title):
+		self.game=game
+		self.listComponant=[]
+		self.TkMenu=Tk()
+		self.TkMenu.withdraw()
+		self.TkMenu.title(title)
+		self.taille=[720,480]
+		self.TkMenu.minsize(self.taille[0],self.taille[1])
 
-    def popComponent(self,componant):
-        self.listComponant.pop(componant)
-        componant.destroy()
+	def popComponent(self,componant):
+		self.listComponant.pop(componant)
+		componant.destroy()
 
-    def addComponent(self,componant):
-        self.listComponant.append(componant)
-        
-    def display(self):
-	    self.TkMenu.deiconify() 
-			
-    def hide(self):
-	    self.TkMenu.withdraw()
+	def addComponent(self,componant):
+		self.listComponant.append(componant)
 		
-    def getMenu(self):
-	    return self
-    
-    def getName(self):
-        return self.TkMenu.title()
+	def display(self):
+		self.TkMenu.deiconify() 
+			
+	def hide(self):
+		self.TkMenu.withdraw()
+		
+	def getMenu(self):
+		return self
+	
+	def getName(self):
+		return self.TkMenu.title()
 
-    def hide(self):
-        self.TkMenu.withdraw()
+	def hide(self):
+		self.TkMenu.withdraw()
 
 class MainMenu(Menu):
 
@@ -42,23 +42,23 @@ class MainMenu(Menu):
 			
 class GameMenu(Menu):
 
-    def __init__(self,game):
-        super().__init__(game,"game menu")
-        self.canvas = Canvas(self.TkMenu, bg="white", width=self.taille[0], height=self.taille[1])
-        self.canvas.grid(row=0,column=0)
-        #self.buttonWin= Button(self.canvas, text="win",command=lambda: game.win()) 
-        #self.buttonWin.grid(row=1,column=1)
+	def __init__(self,game):
+		super().__init__(game,"game menu")
+		self.canvas = Canvas(self.TkMenu, bg="white", width=self.taille[0], height=self.taille[1])
+		self.canvas.grid(row=0,column=0)
+		#self.buttonWin= Button(self.canvas, text="win",command=lambda: game.win()) 
+		#self.buttonWin.grid(row=1,column=1)
 
 class WinMenu(Menu):
 
-    def __init__(self,game):
-        super().__init__(game,"win menu")
-        self.canvas = Canvas(self.TkMenu, bg="white", width=self.taille[0], height=self.taille[1])
-        self.canvas.grid(row=0,column=0)
-        self.label = Label(self.canvas, text="WIN !")
-        self.label.config(font=("Courier", 250))
-        self.label.grid(row=0,column=0)
+	def __init__(self,game):
+		super().__init__(game,"win menu")
+		self.canvas = Canvas(self.TkMenu, bg="white", width=self.taille[0], height=self.taille[1])
+		self.canvas.grid(row=0,column=0)
+		self.label = Label(self.canvas, text="WIN !")
+		self.label.config(font=("Courier", 250))
+		self.label.grid(row=0,column=0)
 
 class Root(Menu):
-    def __init__(self,game):
-        super().__init__(game,"root")
+	def __init__(self,game):
+		super().__init__(game,"root")
