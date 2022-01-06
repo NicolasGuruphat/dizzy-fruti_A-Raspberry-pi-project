49648514a61a98f18d61d72df2100b0db751871a
Noé Favier <noe.favier@etu.univ-lyon1.fr>
2022-01-06 09:24:01 +0100

    Ajout de la vidéo de phil et d'un début de ppt

--- RaspberrySenseHat.py
+++ RaspberrySenseHat.py
@@ -1,10 +1,10 @@
-#from sense_hat import SenseHat
+from sense_hat import SenseHat
 
 class RaspberrySenseHat:
     
     def __init__(self):
         self.lastLed=0
-        #self.sense=SenseHat()
+        self.sense=SenseHat()
         X = [255, 255, 255]
         O = [0,0,0]
         self.matrix = [
@@ -17,12 +17,12 @@ class RaspberrySenseHat:
         O, O, O, O, O, O, O, O,
         O, O, O, O, O, O, O, O
         ]
-        #self.sense.set_pixels(self.question_mark)
+        self.sense.set_pixels(self.question_mark)
 
     def add(self,color, point):
         self.matrix[self.lastLed]=color
         self.lastLed+=1
         print(self.matrix)
-        #self.sense.set_pixels(self.question_mark)
+        self.sense.set_pixels(self.question_mark)
 
         
